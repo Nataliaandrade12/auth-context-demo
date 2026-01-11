3e31bf1e4700a0c1087baf010a6b7e801f94b8b3
Nataliaandrade12 <natalia.fariasdeandrade@wiss-edu.ch>
2026-01-11 15:05:59 +0100

    chore: setup auth context demo project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>🔐 Auth Context Demo</h1>
+            <p>Hier bauen wir unseren AuthContext!</p>
+        </div>
+    );
+}
+
+export default App;
