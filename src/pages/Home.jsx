78356b2145a2f353be762831e49b17af5998f226
Nataliaandrade12 <natalia.fariasdeandrade@wiss-edu.ch>
2026-01-11 16:17:02 +0100

    feat: Add AuthTest component for testing AuthContext

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+// src/pages/Home.jsx
+import React from "react";
+import AuthTest from "../components/auth-test"; // ← NEU!
+
+const Home = () => {
+    return (
+        <div>
+            <p>Teste hier den AuthContext:</p>
+
+            {/* Test Component */}
+            <AuthTest />  {/* ← NEU! */}
+        </div>
+    );
+};
+
+export default Home;
